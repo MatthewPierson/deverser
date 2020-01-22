d9ada3937e480f4b96925daab3bbc9f708ee8914
matty <48230305+MatthewPierson@users.noreply.github.com>
2020-01-23 00:38:29 +1300

    Push to public

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # deverser
 Simple macOS script to dump onboard SHSH for iOS devices
+
+## Requirements
+
+A macOS machine
+A jailbroken device with OpenSSH installed (Specific jailbreak doesn't matter, E.G checkra1n, Unc0ver, chimera, etc)
+img4tool installed (If img4tool is not installed, the script will download the latest release from Tihmstar's repo and install it after getting the users permission)
+
+## Usage
+
+1. Either 'git clone https://github.com/MatthewPierson/deverser.git' or download zip and extract to a folder
+2. 'cd' to the deverser folder and then run 'chmod +x deverser.sh'
+3. Run './deverser.sh'
+4. Follow what the script asks you to do
+
+## Issues/Bugs/Fixes/Improvements
+
+If you have any bugs/issues open an issue [here](https://github.com/MatthewPierson/deverser/issues) with details about your macOS machine (OS version, other basic info), iOS device (iOS version, jailbreak, etc) and details about what is not working. 
+
+Any ideas/fixes/improvements can be sent in a pull request [here](https://github.com/MatthewPierson/deverser/pulls).
+
+## Credits
+
+Matty - [@mosk_i](https://twitter.com/mosk_i) - For writing the script
+Tihmstar - [@tihmstar](https://twitter.com/tihmstar) - For creating img4tool
---
+++ deverser.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if test -f "dump.raw"; then
+    rm -rf dump.raw
+fi
+echo ""
+echo "[!] Welcome to Déverser, a simple script to dump onboard SHSH (Blobs) for iOS devices..."
+echo "[!] This script will allow you to use dumped blobs with futurerestore at a later date (depending on SEP compatibility)..."
+if test -f "/usr/local/bin/img4tool"; then
+    echo "[!] Found img4tool at '/usr/local/bin/img4tool'!"
+else
+    echo "[#] img4tool is not installed, do you want Déverser to download and install img4tool? (If no then the script will close, img4tool is needed)"
+    echo "[*] Please enter 'Yes' or 'No':"
+    read consent
+    if [ $consent == 'Yes' ] | [ $consent == 'yes' ]; then
+        echo "[!] Downloading latest img4tool from Tihmstar's repo..."
+        latestBuild=$(curl --silent "https://api.github.com/repos/tihmstar/img4tool/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        link="https://github.com/tihmstar/img4tool/releases/download/${latestBuild}/buildroot_macos-latest.zip"
+        echo "$link"
+        curl -L "$link" --output img4tool-latest.zip
+        mkdir img4tool
+        unzip -q img4tool-latest.zip -d img4tool
+        echo "[*] Terminal may ask for permission to move the files into '/usr/local/bin' and '/usr/local/include', please enter your password if it does..."
+        cp img4tool/buildroot_macos-latest/usr/local/bin/img4tool /usr/local/bin/img4tool
+        cp -R img4tool/buildroot_macos-latest/usr/local/include/img4tool /usr/local/include
+        chmod +x /usr/local/bin/img4tool
+        rm -rf img4tool-latest.zip
+        rm -rf img4tool/
+        echo "[*] Installed img4tool"
+    elif [ $consent == 'No' ] | [ $consent == 'no' ]; then
+        echo "[#] img4tool is needed for this script to work..."
+        echo "[#] If you want to manually install it, you can download img4tool from 'https://github.com/tihmstar/img4tool/releases/latest' and manually move the files to the correct locations..."
+        exit
+    else
+        echo "[#] Unknown input, assuming to be a variant of 'No'..."
+        echo "[#] img4tool is needed for this script to work..."
+        echo "[#] If you want to manually install it, you can download img4tool from 'https://github.com/tihmstar/img4tool/releases/latest' and manually move the files to the correct locations..."
+        exit
+    fi  
+fi
+echo "[!] Please enter your device's IP address (Found in wifi settings)..."
+read ip
+echo "Device's IP address is ${ip}"
+echo "[*] Assuming given IP to be correct, if connecting to the device fails ensure you entered the IP correctly and have OpenSSh installed..."
+echo "[!] Please enter the device's root password (Default is 'alpine')..."
+ssh root@${ip} "cat /dev/rdisk1 | dd of=dump.raw bs=256 count=$((0x4000))"
+echo "[!] Dumped onboard SHSH to device, about to copy to this machine..."
+echo "[!] Please enter the device's root password again (Default is 'alpine')..."
+scp root@${ip}:dump.raw dump.raw
+if test -f "dump.raw"; then
+    echo ""
+else
+    echo "[#] Error: Failed to to copy 'dump.raw' from device to local machine..."
+    exit
+fi
+echo "[!] Copied dump.raw to this machine, about to convert to SHSH..."
+img4tool --convert -s dumped.shsh dump.raw
+if img4tool -s dumped.shsh | grep -q 'failed'; then
+    echo "[#] Error: Failed to create SHSH from 'dump.raw'..."
+    exit
+fi
+ecid=$(img4tool -s dumped.shsh | grep "ECID" | cut -c13-)
+mv dumped.shsh ${ecid}.dumped.shsh # Allows multiple devices to be dumped as each dump/converted SHSH will have a filename that links the SHSH to the device
+echo "[!] SHSH should be dumped successfully at '${ecid}.dumped.shsh' (The number in the filename is your devices ECID)! Enojy!"
+echo "[@] Written by Matty (@mosk_i)"
+echo ""
